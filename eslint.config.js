4046954cfc916dbce10a8d56b0ed55fbd6492e16
Dessert99 <lsjh1234@naver.com>
2025-09-29 16:25:50 +0900

    Build:  eslint-plugin-tailwind CSS 실패

--- eslint.config.js
+++ eslint.config.js
@@ -5,16 +5,15 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import tailwind from 'eslint-plugin-tailwindcss';
-import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default defineConfig([
   globalIgnores(['dist']),
+  js.configs.recommended, // ESLint 기본 권장
+  ...tailwind.configs['flat/recommended'], // Tailwind 플러그인 권장(배열)
+  ...tseslint.configs.recommended, // TypeScript 권장(배열)
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
-      js.configs.recommended, // ESLint 기본 권장
-      ...tseslint.configs.recommended, // TypeScript 권장(배열)
-      ...tailwind.configs['flat/recommended'], // Tailwind 플러그인 권장(배열)
       reactHooks.configs['recommended-latest'], // React Hooks 권장
       reactRefresh.configs.vite, // Vite 환경에서 React Refresh
     ],
@@ -26,5 +25,4 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-  eslintConfigPrettier,
 ]);
