37173e648e65606b4de639a9dc7965a394167eba
Dessert99 <lsjh1234@naver.com>
2025-10-23 14:36:47 +0900

    build: shadcn ui 설치
    - 추가로 Button ui 설치해서 사용했다.

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@/components/ui/button';
+
 function MainPage() {
-  return <div>메인페이지</div>;
+  return (
+    <div>
+      메인페이지
+      <Button>안녕</Button>
+    </div>
+  );
 }
 
 export default MainPage;
