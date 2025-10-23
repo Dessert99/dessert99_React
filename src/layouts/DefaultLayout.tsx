86ee4b3a09bb4219f0fcf6347e8465a4fdf96d05
Dessert99 <lsjh1234@naver.com>
2025-10-23 17:54:02 +0900

    refactor(router): 라우터 역할만 하는 App.tsx를 /routes/index.tsx로 변경
    - react-router v6부터 도입된 Data모드로 구성
    - App.tsx에  존재하던 Toaster을 main.tsx로 옮김.
    - 라우터 역할만 하던 App.tsx를 삭제하고 reoutes/index.tsx를 만들어 RouterProvider연결했다.
    - App.tsx에 존재하던 Layout을 따로 분리하여 DefaultLayout라는 이름으로 변경했다.

---
+++ src/layouts/DefaultLayout.tsx
@@ -0,0 +1,13 @@
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import { Outlet } from 'react-router';
+
+export default function DefaultLayout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet /> {/*자식 요소를 가리킨다. 페이지 컴포넌트의 렌더링 위치를 결정해준다. */}
+      <Footer />
+    </>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,29 +1,17 @@
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router';
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
-import ProjectPage from '@/pages/ProjectPage';
-import MainPage from '@/pages/MainPage';
+import DefaultLayout from '@/layouts/DefaultLayout';
 import AboutPage from '@/pages/AboutPage';
 import BoardPage from '@/pages/BoardPage';
-import LeadershipPage from '@/pages/LeadershipPage';
-import ServicePage from '@/pages/ServicePage';
 import ContactPage from '@/pages/ContactPage';
-import { Toaster } from './components/ui/sonner';
-
-function Layout() {
-  return (
-    <>
-      <Navbar />
-      <Outlet /> {/*자식 요소를 가리킨다. 페이지 컴포넌트의 렌더링 위치를 결정해준다. */}
-      <Footer />
-    </>
-  );
-}
+import LeadershipPage from '@/pages/LeadershipPage';
+import MainPage from '@/pages/MainPage';
+import ProjectPage from '@/pages/ProjectPage';
+import ServicePage from '@/pages/ServicePage';
+import { createBrowserRouter, RouterProvider } from 'react-router';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />,
+    element: <DefaultLayout />,
     children: [
       {
         index: true, //루트 경로와 동일
@@ -39,13 +27,12 @@ const router = createBrowserRouter([
   },
 ]);
 
-function App() {
+function Router() {
   return (
     <>
-      <Toaster />
       <RouterProvider router={router} />
     </>
   );
 }
 
-export default App;
+export default Router;
