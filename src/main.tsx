9e3fd454a8e1193facf442f0e864ac321a4dcc2e
Dessert99 <lsjh1234@naver.com>
2025-10-24 15:10:28 +0900

    feat(query): 리액트 쿼리 main.tsx에 등록
    - ReactQueryDevtools도 함께 등록하였다.

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,15 @@ import { createRoot } from 'react-dom/client';
 import '@/index.css';
 import Router from './routes';
 import { Toaster } from 'sonner';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient(); // 모든 서버 상태를 저장하는 스토어
 
 createRoot(document.getElementById('root')!).render(
-  <>
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools />
     <Router />
     <Toaster />
-  </>,
+  </QueryClientProvider>,
 );
