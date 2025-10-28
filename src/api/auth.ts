dbc0b317bd631540e653585cf67d541ec51e2599
Dessert99 <lsjh1234@naver.com>
2025-10-28 20:43:47 +0900

    feat(auth): 로그인 api 로직 signInWithPassword 추가
    - 추후 소셜 로그인 도입을 위해 signInWithPassword라고 명칭

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,12 +1,18 @@
 import supabase from '@/lib/supabase';
 
-interface SignUpTpye {
+interface WithPasswordType {
   email: string;
   password: string;
 }
 
-export const signUp = async (body: SignUpTpye) => {
+export const signUp = async (body: WithPasswordType) => {
   const { data, error } = await supabase.auth.signUp(body);
   if (error) throw error;
   return data; //회원가입 결과 (user,sesstion 정보 반환)
+};
+
+export const signInWithPassword = async (body: WithPasswordType) => {
+  const { data, error } = await supabase.auth.signInWithPassword(body);
+  if (error) throw error;
+  return data;
 };
