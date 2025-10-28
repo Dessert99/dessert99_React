3a26754deac45d692d6fdda8d9d8cab8b155ce28
Dessert99 <lsjh1234@naver.com>
2025-10-28 20:09:49 +0900

    feat(auth): 회원가입 페이지 UI뼈대

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,5 +1,52 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useState } from 'react';
+import { Link } from 'react-router';
+
 const SignUpPage = () => {
-  return <div>회원가입 페이지</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async () => {
+    if (email.trim() === '' || password.trim() === '') return;
+  };
+
+  return (
+    <div className='flex flex-col gap-8'>
+      <div className='text-2xl font-bold'>회원가입 페이지</div>
+      <div className='flex flex-col gap-3'>
+        <Input
+          className='py-6'
+          type='email'
+          placeholder='이메일을 입력해주세요.'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          className='py-6'
+          type='password'
+          placeholder='비밀번호'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <Button
+          className='w-full'
+          onClick={handleSubmit}>
+          회원가입
+        </Button>
+      </div>
+      <div>
+        <Link
+          to={'/sign-in'}
+          className='text-muted-foreground hover:underline'>
+          {/* muted-foreground: 배경(background)과 대비되지만 덜 눈에 띄는 색 */}
+          이미 아이디가 있다면? 로그인으로 가기
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;
