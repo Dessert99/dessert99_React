ad9ecdd77fb971db26412507db5a732ae54216a3
Dessert99 <lsjh1234@naver.com>
2025-10-28 20:44:39 +0900

    feat(auth): 로그인 페이지 UI 및 로그인 요청 연결

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,5 +1,56 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useSignInWithPassword } from '@/hooks/mutations/useSignInWithPassword';
+
+import { useState } from 'react';
+import { Link } from 'react-router';
+
 const SignInPage = () => {
-  return <div>로그인페이지</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { mutate: signInWithPassword } = useSignInWithPassword();
+
+  const handleSubmit = () => {
+    if (email.trim() === '' || password.trim() === '') return;
+    signInWithPassword({ email, password });
+  };
+
+  return (
+    <div className='flex flex-col gap-8'>
+      <div className='text-2xl font-bold'>로그인 페이지</div>
+      <div className='flex flex-col gap-3'>
+        <Input
+          className='py-6'
+          type='email'
+          placeholder='이메일을 입력해주세요.'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          className='py-6'
+          type='password'
+          placeholder='비밀번호'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <Button
+          className='w-full'
+          onClick={handleSubmit}>
+          로그인
+        </Button>
+      </div>
+      <div>
+        <Link
+          to={'/sign-up'}
+          className='text-muted-foreground hover:underline'>
+          {/* muted-foreground: 배경(background)과 대비되지만 덜 눈에 띄는 색 */}첫 방문이라면?
+          회원가입 가기
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default SignInPage;
