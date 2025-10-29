b0b3518387cf7a44a37b8b5f4ae60c934755452c
Dessert99 <lsjh1234@naver.com>
2025-10-30 00:04:27 +0900

    feat(auth): 소셜 로그인(깃허브) api 추가

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,4 +1,5 @@
 import supabase from '@/lib/supabase';
+import type { Provider } from '@supabase/supabase-js';
 
 interface WithPasswordType {
   email: string;
@@ -14,5 +15,14 @@ export const signUp = async (body: WithPasswordType) => {
 export const signInWithPassword = async (body: WithPasswordType) => {
   const { data, error } = await supabase.auth.signInWithPassword(body);
   if (error) throw error;
+  return data;
+};
+
+//소셜 로그인 api
+export const signInWithOAuth = async (provider: Provider) => {
+  // provider라는 인증을 제공할 웹 서비스의 이름을 전달해야 한다. Provider타입은 다양한 외부 서비스들이 유니온으로 묶여있다.
+  const { error, data } = await supabase.auth.signInWithOAuth({ provider });
+  if (error) throw error;
+
   return data;
 };
