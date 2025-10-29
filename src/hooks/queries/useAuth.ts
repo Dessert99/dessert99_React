21c52e8cfa6c45147360ca77793fe179895df402
Dessert99 <lsjh1234@naver.com>
2025-10-30 00:55:20 +0900

    feat(auth): 로그인 실패 에러 핸들링

--- src/hooks/queries/useAuth.ts
+++ src/hooks/queries/useAuth.ts
@@ -2,6 +2,7 @@ import { signInWithOAuth } from '@/api/auth';
 import { useMutation } from '@tanstack/react-query';
 import { signInWithPassword } from '@/api/auth';
 import { signUp } from '@/api/auth';
+import { toast } from 'sonner';
 
 export function useSignInWithOAuth() {
   return useMutation({
@@ -12,6 +13,14 @@ export function useSignInWithOAuth() {
 export function useSignInWithPassword() {
   return useMutation({
     mutationFn: signInWithPassword,
+
+    //signInWithPassword에서 던져진 error객체는 여기로 온다.
+    onError: (error) => {
+      console.error(error);
+      toast.error(error.message, {
+        position: 'top-center',
+      });
+    },
   });
 }
 
