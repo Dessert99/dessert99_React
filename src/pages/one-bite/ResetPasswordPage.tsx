eb35e1434dd5582d8a031406db57a37624cded0e
Dessert99 <lsjh1234@naver.com>
2025-11-06 18:29:33 +0900

    feat(ui): 비밀번호 재설정 기능에서 리디렉션에 활용될 ResetPasswordPage UI 구현
    #2

--- src/pages/one-bite/ResetPasswordPage.tsx
+++ src/pages/one-bite/ResetPasswordPage.tsx
@@ -1,5 +1,20 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
 const ResetPasswordPage = () => {
-  return <div>비밀번호 초기화 페이지</div>;
+  return (
+    <div className='flex flex-col gap-10'>
+      <div className='flex flex-col gap-2'>
+        <div className='text-xl font-bold'>비밀번호 재설정하기</div>
+        <div className='text-muted-foreground'>새로운 비밀번호를 입력해주세요</div>
+      </div>
+      <Input
+        className='py-6'
+        placeholder='password'
+      />
+      <Button className='w-full'>비밀번호 변경하기</Button>
+    </div>
+  );
 };
 
 export default ResetPasswordPage;
