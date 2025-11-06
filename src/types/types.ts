ccb59c4c883d88a3704ee05aab607c4586ce86fa
Dessert99 <lsjh1234@naver.com>
2025-11-06 22:18:24 +0900

    feat(type): 프로필 테이블 타입 가져오기
    - 타입 정제까지 진행
    
    #2

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,6 +2,7 @@ import { type Database } from './database.types';
 
 //타입 정제화
 export type PostEntity = Database['public']['Tables']['post']['Row'];
+export type ProfileEntity = Database['public']['Tables']['profile']['Row'];
 
 export type UseMutationCallback = {
   onSuccess?: () => void;
