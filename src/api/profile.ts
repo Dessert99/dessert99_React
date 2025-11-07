798af7b032cafe14fabb49a841c344287a672508
Dessert99 <lsjh1234@naver.com>
2025-11-07 14:15:04 +0900

    feat(profile): 프로필 자동 생성 api 기능 추가
    - 닉네임은 getRandomNickname함수에 의해 자동으로 만들어진다
    - isMine값으로 나한테만 적용되도록 한다.
    - PostgrestError타입의 "PGRST116" 에러의 경우에 프로필 생성 함수를 실행한다.
    
    #2

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -1,4 +1,5 @@
 import supabase from '@/lib/supabase';
+import { getRandomNickname } from '@/utils/randomNickname';
 
 // 회원가입 이후 기존 프로필 정보가 있는지 조회하는 api
 export const fetchProfile = async (userId: string) => {
@@ -8,6 +9,25 @@ export const fetchProfile = async (userId: string) => {
     .select('*') //  어떤 칼럼 데이터를 가져올 것인가 (*는 모든 데이터)
     .eq('id', userId) // id = userId인 데이터만 불러오기
     .single(); // 조건에 부합하는 단 하나의 데이터만 불러오기
+
+  if (error) throw error;
+  return data;
+};
+
+// 프로필 조회 요청에서 데이터가 없다는 오류가 발생했을 때 호출하는 APi
+export const createProfile = async (userId: string) => {
+  const { data, error } = await supabase
+    .from('profile')
+    // 'profile' 테이블에 새로운 데이터를 삽입(insert)합니다.
+    .insert({
+      id: userId,
+      nickname: getRandomNickname(),
+      bio: '',
+    })
+    .select() // 데이터를 삽입한 후, 방금 삽입된 그 행(row)의 데이터를 반환받기 위해 select()를 호출합니다
+    // insert().select()는 기본적으로 배열을 반환하지만,
+    // .single()을 추가하여 방금 삽입된 단 하나의 객체(data)만 반환하도록 합니다.
+    .single();
 
   if (error) throw error;
   return data;
--- src/hooks/queries/useProfile.ts
+++ src/hooks/queries/useProfile.ts
@@ -1,11 +1,29 @@
-import { fetchProfile } from '@/api/profile';
+import { createProfile, fetchProfile } from '@/api/profile';
 import { QUERY_KEYS } from '@/constants/queryKeys';
+import { useSession } from '@/store/authStore';
+import type { PostgrestError } from '@supabase/supabase-js';
 import { useQuery } from '@tanstack/react-query';
 
 export function useProfileData(userId?: string) {
+  const session = useSession();
+
+  const isMine = session?.user.id === userId; // 내 프로필이 없을 때만 프로필을 생성하기 위해 추가
+
   return useQuery({
     queryKey: QUERY_KEYS.profile.byId(userId!), // userId는 절대 null이나 undefined가 아니라고 TypeScript에게 단언하는(!) 구문이다.
-    queryFn: () => fetchProfile(userId!),
+    queryFn: async () => {
+      try {
+        // 프로필이 존재한다면 불러온다.
+        const profile = await fetchProfile(userId!);
+        return profile;
+      } catch (error) {
+        // 프로필 정보가 없다면, PostgrestError타입의 "PGRST116" 에러가 발생한다. 이때 새로운 프로필을 자동으로 생성해준다.
+        if (isMine && (error as PostgrestError).code === 'PGRST116') {
+          return await createProfile(userId!);
+        }
+        throw error; // 다른 에러라면 던지기
+      }
+    },
     enabled: !!userId, // 유저 아이디가 없으면 아무 일도 일어나지 않는다. !! 연산자는 어떤 값이든 명시적인 true 또는 false 값으로 변환하는 역할
   });
 }
