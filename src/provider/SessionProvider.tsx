39e4c013519c0bdec82cb8c3504a5d0f23ed3678
Dessert99 <lsjh1234@naver.com>
2025-11-07 14:15:51 +0900

    feat(profile): SessionProvider에 useProfileData훅 연결
    - isLoading값을 사용해 프로필 정보를 가져올 때만 로딩 화면을 보여준다.

--- src/provider/SessionProvider.tsx
+++ src/provider/SessionProvider.tsx
@@ -1,12 +1,21 @@
 import GlobalLoader from '@/components/one-bite/GlobalLoader';
+import { useProfileData } from '@/hooks/queries/useProfile';
 import supabase from '@/lib/supabase';
-import { useIsLoaded, useSetSession } from '@/store/authStore';
+import { useIsLoaded, useSession, useSetSession } from '@/store/authStore';
 import { useEffect, type ReactNode } from 'react';
 
 // 리액트 컴포넌트를 children으로 받는다.
 export default function SessionProvider({ children }: { children: ReactNode }) {
+  const session = useSession();
   const setSession = useSetSession();
   const isLoaded = useIsLoaded();
+
+  // isLoading은 queryfn이 실행 중일 때에만 true가 된다. ⭐️ isPending으로 하면 계속 로딩 중
+  const {
+    data: profile,
+    isLoading: isProfileLoading,
+    isPending,
+  } = useProfileData(session?.user.id); // 사용자가 로그인 했을 때 프로필 정보가 있다면 useProfileData가 실행될 것.
 
   useEffect(() => {
     // onAuthStateChange: 세션 데이터가 변할 때마다 호출된다.
@@ -17,6 +26,7 @@ export default function SessionProvider({ children }: { children: ReactNode }) {
   }, []);
 
   if (!isLoaded) return <GlobalLoader />;
+  if (isProfileLoading) return <GlobalLoader />; // 프로필 정보 가져올 때는 로딩 화면
 
   return children;
 }
