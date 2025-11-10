a08d0d5a1ea864117487c8b4ca56853d12082cf2
Dessert99 <lsjh1234@naver.com>
2025-11-10 18:16:06 +0900

    feat(shadcn): Button컴포넌트 실습
    #3

--- src/pages/default/ShadcnPage.tsx
+++ src/pages/default/ShadcnPage.tsx
@@ -1,14 +1,18 @@
-import Shadcn_Calendar from '@/components/default/Shadcn_Calendar';
-import Shadcn_InputOTP from '@/components/default/Shadcn_InputOTP';
+import Shadcn_Button from '@/components/shadcn/Shadcn_Button';
 
 const ShadcnPage = () => {
   return (
-    <>
-      <div>
-        <Shadcn_Calendar />
-        <Shadcn_InputOTP />
-      </div>
-    </>
+    <div className='mx-auto flex max-w-6xl flex-col border'>
+      <header className='border-2 p-5 text-center'>
+        <div className='text-3xl font-bold'>여긴 shadcn를 연습하는 페이지입니다.</div>
+      </header>
+      <main className='px-4'>
+        <div className='flex flex-col gap-5 border-2 border-black p-5'>
+          <div className='bold text-2xl'>첫 번째: Button</div>
+          <Shadcn_Button />
+        </div>
+      </main>
+    </div>
   );
 };
 
