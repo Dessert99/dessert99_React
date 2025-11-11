f9854981eeacd93dd3e9e6a1810fbce2bfe3eea9
Dessert99 <lsjh1234@naver.com>
2025-11-11 10:36:24 +0900

    feat(layout): 멋사 레이아웃 추가
    #7

---
+++ src/layouts/LikeLionLayout.tsx
@@ -0,0 +1,38 @@
+import { Link, Outlet } from 'react-router';
+import { House, NotebookPen, Coffee, User } from 'lucide-react';
+
+export default function LikeLionLayout() {
+  return (
+    <div className='mx-auto flex min-h-screen max-w-xl flex-col border-2 border-black'>
+      <main className='flex w-full flex-1 flex-col'>
+        <Outlet />
+      </main>
+      <footer className='flex w-full justify-evenly border-t-2 p-2'>
+        <Link to={'/likelion'}>
+          <div className='flex flex-col items-center text-sm'>
+            <House />홈
+          </div>
+        </Link>
+        <Link to={'/likelion/study'}>
+          <div className='flex flex-col items-center text-sm'>
+            <NotebookPen />
+            스터디룸
+          </div>
+        </Link>
+        <Link to={'/likelion/collection'}>
+          <div className='flex flex-col items-center text-sm'>
+            <Coffee />
+            컬렉션
+          </div>
+        </Link>
+        <div className='flex flex-col items-center text-sm'>
+          <User />내 프로필
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+/*
+
+*/
